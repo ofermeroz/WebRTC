a29c2b0cd66406d2532ee36ef7bd0bd0fd01c927
Ofer Meroz <ofermeroz@gmail.com>
2020-10-23 23:44:21 +0300

    Updated Package.swift file and added .swiftpm files.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "WebRTC",
-    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -17,12 +16,10 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-                    name: "WebRTC",
-                    url: "https://dl.dropboxusercontent.com/s/rit06wgfug0sjad/WebRTC.zip?dl=0",
-                    checksum: "37db36570f78f8bd428fbf57a1034697320d6e4d752bcd00d2dbab413c87a11f"
-                ),
+            name: "WebRTC",
+            url: "https://dl.dropboxusercontent.com/s/rit06wgfug0sjad/WebRTC.zip?dl=0",
+            checksum: "37db36570f78f8bd428fbf57a1034697320d6e4d752bcd00d2dbab413c87a11f"
+        )
     ]
 )
